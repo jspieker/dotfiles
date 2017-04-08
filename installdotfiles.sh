891a0421c97680b833178f6d4f0d33aef084b445
Jannik Spieker <jannikspieker@icloud.com>
2017-04-08 17:01:57 +0200

    feat: add common dotfiles and dotfile install script

---
+++ installdotfiles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+##################
+# Create symlinks from homedir to any desired dotfiles
+##################
+
+dir=~/Development/config/dotfiles
+backupdir=~/dotfiles_backup
+files="bash_profile vimrc zshrc oh-my-zsh vim atom"
+
+echo "Creating $backupdir etc for backup of any existing dotfiles in ~"
+mkdir -p $backupdir
+mkdir -p $backupdir .oh-my-zsh
+mkdir -p $backupdir .vim
+mkdir -p $backupdir .atom
+
+echo "Changing to the $dir directory"
+cd $dir
+
+echo "Moving any existing dotfiles from ~ to $backupdir"
+for file in $files; do
+  mv ~/.$file $backupdir
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
+done
